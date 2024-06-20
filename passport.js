69b5775c8024387b4daeb76b41f0addca32bc75b
vishnu <vishnuprasadvv24@gamil.comgit config --global user.name vishnuprasadvv>
2024-06-20 15:06:07 +0530

    google auth url changed test 2

--- passport.js
+++ passport.js
@@ -6,7 +6,7 @@ passport.use(
     new GoogleStrategy({
         clientID:process.env.CLIENT_ID,
         clientSecret:process.env.CLIENT_SECRET,
-        callbackURL:"https://elektronix.co.in/auth/google/callback",
+        callbackURL:"http://localhost:5000/auth/google/callback",
         scope:['profile','email']
     },
     (accessToken, refreshToken, profile, done) => {
